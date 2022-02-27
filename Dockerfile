a5baa774ee4ae7574b051d7f2f2b9ebebb0840ad
Pascal Rinker <info@kronova.net>
2022-02-27 18:32:31 +0100

    [TASK] Update docker-compose example, fix setup and more

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:10-alpine
+FROM node:12-alpine
 
 LABEL vendor="kronov.net"
 LABEL maintainer="info@kronova.net"
--- setup.js
+++ setup.js
@@ -1,41 +1,41 @@
 // setup process that creates a client id and client secret for your Google Actions project
 
-const redisClient = require("./config/config").createRedisClient();
-const prompts = require("prompts");
+const redisClient = require('./config/config').createRedisClient();
+const prompts = require('prompts');
 
 (async () => {
   redisClient.connect();
 
   let client = {
-    clientId: require("crypto").randomBytes(128).toString("hex"),
-    clientSecret: require("crypto").randomBytes(128).toString("hex"),
-    grants: ["authorization_code", "refresh_token"],
+    clientId: require('crypto').randomBytes(64).toString('hex'),
+    clientSecret: require('crypto').randomBytes(128).toString('hex'),
+    grants: ['authorization_code', 'refresh_token'],
     accessTokenLifetime: 3600,
-    projectId: "",
+    projectId: '',
     redirectUris: [],
   };
 
-  console.log("Welcome to the Better Ring Doorbell Project by kronova.net!");
+  console.log('Welcome to the Better Ring Doorbell Project by kronova.net!');
   console.log(
-    "Use this setup to create your own Google Action for your Ring Doorbell(s). Read the README.md before you continue the setup!"
+    'Use this setup to create your own Google Action for your Ring Doorbell(s). Read the README.md before you continue the setup!',
   );
 
   const response = await prompts([
     {
-      type: "text",
-      name: "projectId",
-      message: "Google Actions Project ID",
+      type: 'text',
+      name: 'projectId',
+      message: 'Google Actions Project ID',
     },
     {
-      type: "text",
-      name: "clientId",
-      message: "Client ID for Google Actions",
+      type: 'text',
+      name: 'clientId',
+      message: 'Client ID for Google Actions',
       initial: client.clientId,
     },
     {
-      type: "text",
-      name: "clientSecret",
-      message: "Client Secret for Google Actions",
+      type: 'text',
+      name: 'clientSecret',
+      message: 'Client Secret for Google Actions',
       initial: client.clientSecret,
     },
   ]);
@@ -48,22 +48,17 @@ const prompts = require("prompts");
     `https://oauth-redirect-sandbox.googleusercontent.com/r/${response.projectId}`,
   ];
 
-  console.log(
-    "The next step is the authorization with ring.com. Please follow the instructions:"
-  );
-  const refreshTokenApi = require("ring-client-api/lib/api/refresh-token");
+  console.log('The next step is the authorization with ring.com. Please follow the instructions:');
+  const refreshTokenApi = require('ring-client-api/lib/api/refresh-token');
 
   const refreshToken = await refreshTokenApi.acquireRefreshToken();
 
-  console.log("Save credentials...");
+  console.log('Save credentials...');
   await redisClient.hSet(`client:${client.clientId}`, client);
-  console.log("Save ring.com refresh token...");
-  await redisClient.hSet("user:1", { id: 1, ringRefreshToken: refreshToken });
-  await redisClient.hSet(`a`);
+  console.log('Save ring.com refresh token...');
+  await redisClient.hSet('user:1', { id: 1, ringRefreshToken: refreshToken });
 
-  console.log(
-    "Use the following information to setup the account linking of your Google Action:"
-  );
+  console.log('Use the following information to setup the account linking of your Google Action:');
   console.log(`Client ID: ${client.clientId}`);
   console.log(`Client Secret: ${client.clientSecret}`);
 
